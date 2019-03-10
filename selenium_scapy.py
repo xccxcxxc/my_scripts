45afe62fffe13c09d082fdf9d2ac588962d1c0cc
xccxcxxc <chenx212@gmail.com>
2019-03-10 21:41:40 +0800

    加入1次重连，解决 selenium 偶尔断连问题

--- selenium_scapy.py
+++ selenium_scapy.py
@@ -7,17 +7,19 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(30)
+driver.implicitly_wait(10)
 
-url = 'https://18h.animezilla.com/manga/3403/69'  # starting url
-basedir = '/Users/cx/Downloads/女配角'
+url = 'https://18h.animezilla.com/manga/784/1'  # starting url
+basedir = '/Users/cx/Downloads/强制操作'
 os.makedirs(basedir, exist_ok=True)
 
 while True:
     # TODO: Download the page.
     # 必须先用 selenium 框架打开一次才能下载到清晰图，用 requests 无论如何伪装，得到都是缩略图
     driver.get(url)
-    assert "H" in driver.title
+    if "H" not in driver.title:
+        driver.get(url)
+        assert "H" in driver.title
 
     res = requests.get(url)
     if res.status_code != 200:
@@ -31,6 +33,7 @@ while True:
 
     if len(comicElem) == 0:
         print('Could not find comic image.')
+        break
     else:
         comicUrl = comicElem[0].get('src')
 
@@ -38,7 +41,9 @@ while True:
         print('Downloading image %s...' % comicUrl)
 
         driver.get(comicUrl)
-        assert str(os.path.basename(comicUrl)) in driver.title
+        if str(os.path.basename(comicUrl)) not in driver.title:
+            driver.get(comicUrl)
+            assert str(os.path.basename(comicUrl)) in driver.title
 
         # TODO: Save the image to downloads.
         pyautogui.rightClick(720, 450)
