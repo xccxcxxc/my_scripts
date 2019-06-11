e4f058930772946a29271d1c21c464ed53b9498a
xccxcxxc <chenx212@gmail.com>
2019-06-11 11:08:59 +0800

    excel 处理

--- csv处理.py
+++ csv处理.py
@@ -5,24 +5,24 @@ import os
 
 oldFile = 'test.csv'
 
-for oldFile in os.listdir('.'):
-    newTest = []
-    with open(oldFile, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if reader.line_num == 1:
-                newTest.append(row)
-                continue
-            ipRex = re.compile(r'(\d{1,3}\.){1,3}\d{1,3}')
-            ip = ipRex.search(row[-1])
-            if ip is not None:
-                row[-1] = ip.group()
-                newTest.append(row)
+
+newTest = []
+with open(oldFile, 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if reader.line_num == 1:
+            newTest.append(row)
+            continue
+        ipRex = re.compile(r'(\d{1,3}\.){1,3}\d{1,3}')
+        ip = ipRex.search(row[-1])
+        if ip is not None:
+            row[-1] = ip.group()
+            newTest.append(row)
 
 
-    with open('new'+oldFile, 'w', encoding='utf-8-sig') as f:
-        writer = csv.writer(f)
-        '''for row in newTest:
-            print(row)
-            writer.writerow(row)'''
-        writer.writerows(newTest)
+with open('new'+oldFile, 'w', encoding='utf-8-sig') as f:
+    writer = csv.writer(f)
+    '''for row in newTest:
+        print(row)
+        writer.writerow(row)'''
+    writer.writerows(newTest)
--- excel处理.py
+++ excel处理.py
@@ -1,19 +1,22 @@
-from openpyxl import load_workbook
-from re import compile
+import openpyxl
+import re
+import os
 
 
 excelFile = 'test.xlsx'
 sheetName = 'test'
 
-wb = load_workbook(excelFile)
-sheet = wb[sheetName]
+for excelFile in os.listdir('.'):
+    if os.path.splitext(excelFile)[-1] == '.xlsx':
+        wb = openpyxl.load_workbook(excelFile)
+        #sheetnames = wb.get_sheet_names()
+        sheet = wb[wb.sheetnames[0]]
 
+        for rowNum in range(2, sheet.max_row):
+            # print(sheet['F' + str(rowNum)].value)
+            ipRex = re.compile(r'(\d{1,3}\.){1,3}\d{1,3}')
+            ip = ipRex.search(sheet['F'+str(rowNum)].value)
+            # print(ip.group())
+            sheet.cell(row=rowNum, column=7).value = ip.group()
 
-for rowNum in range(2, sheet.max_row):
-    #print(sheet['F' + str(rowNum)].value)
-    ipRex = compile(r'(\d{1,3}\.){1,3}\d{1,3}')
-    ip = ipRex.search(sheet['F'+str(rowNum)].value)
-    #print(ip.group())
-    sheet.cell(row=rowNum, column=7).value = ip.group()
-
-wb.save(excelFile)
+        wb.save(excelFile)
