c76194336bb5c3d784836dd67074933987bd6891
xccxcxxc <chenx212@gmail.com>
2019-06-11 08:00:03 +0800

    Init

--- csv处理.py
+++ csv处理.py
@@ -1,24 +1,28 @@
 import csv
 import re
+import os
 
 
 oldFile = 'test.csv'
-newFile = 'newTest.csv'
 
-newTest = []
-with open(oldFile, 'r', encoding='utf-8') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        ipRex = re.compile(r'(\d{1,3}\.){1,3}\d{1,3}')
-        ip = ipRex.search(row[-1])
-        if ip is not None:
-            row[-1] = ip.group()
-        newTest.append(row)
+for oldFile in os.listdir('.'):
+    newTest = []
+    with open(oldFile, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if reader.line_num == 1:
+                newTest.append(row)
+                continue
+            ipRex = re.compile(r'(\d{1,3}\.){1,3}\d{1,3}')
+            ip = ipRex.search(row[-1])
+            if ip is not None:
+                row[-1] = ip.group()
+                newTest.append(row)
 
 
-with open(newFile, 'w', encoding='utf-8-sig') as f:
-    writer = csv.writer(f)
-    '''for row in newTest:
-        print(row)
-        writer.writerow(row)'''
-    writer.writerows(newTest)
+    with open('new'+oldFile, 'w', encoding='utf-8-sig') as f:
+        writer = csv.writer(f)
+        '''for row in newTest:
+            print(row)
+            writer.writerow(row)'''
+        writer.writerows(newTest)
