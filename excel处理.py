98ade6587cb86e67f0636c66abc9a0d6388f9282
xccxcxxc <chenx212@gmail.com>
2019-06-11 14:56:58 +0800

    excel 处理添加提取唯一 ip

--- excel处理.py
+++ excel处理.py
@@ -3,6 +3,7 @@ import re
 import os
 import progressbar
 
+uniqueIp = set()
 print("开始处理文件夹中所有 .xlsx 文件……\r\n")
 for excelFile in os.listdir('.'):
     if os.path.splitext(excelFile)[-1] == '.xlsx':
@@ -10,15 +11,19 @@ for excelFile in os.listdir('.'):
         wb = openpyxl.load_workbook(excelFile)
         #sheetnames = wb.get_sheet_names()
         sheet = wb[wb.sheetnames[0]]
-        print('\r\n提取 {} 中的ip：'.format(excelFile))
 
-        # 加上进度条显示
+        print('\r\n提取 {} 中的ip：'.format(excelFile))
         for rowNum in progressbar.progressbar(range(2, sheet.max_row)):
             # print(sheet['F' + str(rowNum)].value)
             ipRex = re.compile(r'(\d{1,3}\.){1,3}\d{1,3}')
             ip = ipRex.search(sheet['F'+str(rowNum)].value)
             # print(ip.group())
             sheet.cell(row=rowNum, column=7).value = ip.group()
+
+            # 同时保存唯一ip
+            if ip.group() not in uniqueIp:
+                sheet.cell(row=rowNum, column=8).value = ip.group()
+                uniqueIp.add(ip.group())
         print('保存 {} 文件……'.format(excelFile))
         wb.save(excelFile)
 
