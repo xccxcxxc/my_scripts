8d0151b0395caef3331a3661d183384f365a344e
xccxcxxc <chenx212@gmail.com>
2020-05-21 23:10:59 +0800

    批处理 word 文件

--- 1os_操作所有文件.py
+++ 1os_操作所有文件.py
@@ -4,11 +4,14 @@
 """
 import os
 import shutil
-import re
+import csv
+
 
 # os.getcwd 有可能是执行命令的目录而不是文件所在目录
 #base_dir = os.path.abspath(os.path.dirname(__file__))
-base_dir = "d:\\20机关党委"
+#base_dir = "d:\\20机关党委"
+base_dir = "/Users/cx/Downloads/北京海关"
+oldFile = 'test.csv'
 
 # 移动文件
 def move_to_current_dir(foldername, file):
@@ -40,9 +43,7 @@ def do_something_to_file(foldername, file):
     # copy_to_current_dir(foldername, file)
     #delete_file(foldername, file, ext='.pdf')
 
-    # 打印特定后缀的文件
-    if os.path.splitext(file)[-1] == '.txt':
-        print(f'fold:{foldername}, file:{file}, name:{os.path.splitext(file)[0]}')
+
 
     if os.path.splitext(file)[-1] == '.txt':
         absfile = os.path.join(foldername, file)
@@ -58,14 +59,104 @@ def do_something_to_file(foldername, file):
             i += 1
 
         # 以下为高度模板定制化内容，都是死的
-        index = list_txt.index("1．姓名：")
-        name = list_txt[index+1]
+        name = ""
+        if "1．姓名：" in list_txt:
+            index = list_txt.index("1．姓名：")
+            name = list_txt[index+1]
+
         sex = ""
         if "性别：" in list_txt:
             index = list_txt.index("性别：")
             sex = list_txt[index+1]
-        print(f'name:{name} sex:{sex}')
 
+        birth = ""
+        if "出生年月：" in list_txt:
+            index = list_txt.index("出生年月：")
+            birth = list_txt[index + 1]
+
+        hometown = ""
+        if "籍贯：" in list_txt:
+            index = list_txt.index("籍贯：")
+            hometown = list_txt[index+1]
+
+        id = ""
+        if "身份证号：" in list_txt:
+            index = list_txt.index("身份证号：")
+            id = 'ID:' + list_txt[index + 1]
+
+        height = ""
+        if "2．身高：" in list_txt:
+            index = list_txt.index("2．身高：")
+            height = list_txt[index + 1]
+
+        weight = ""
+        if "体重：" in list_txt:
+            index = list_txt.index("体重：")
+            weight = list_txt[index + 1]
+
+        blood = ""
+        if "血型：" in list_txt:
+            index = list_txt.index("血型：")
+            blood = list_txt[index + 1]
+
+        department = ""
+        bj_id = ""
+        if "关员号：" in list_txt:
+            bj_id_index = list_txt.index("关员号：")
+            bj_id = list_txt[bj_id_index + 1]
+            if "3．所在单位及职务职级：" in list_txt:
+                index = list_txt.index("3．所在单位及职务职级：")
+                department = list_txt[index + 1]
+                index+=1
+                while index<bj_id_index-1:
+                    department = department + " " + list_txt[index+1]
+                    index += 1
+
+        disease_history = []
+        if "4." in list_txt:
+            index = list_txt.index("4.")
+            disease_history += list_txt[(index+1):(index+5)]
+
+        healthy = ''
+        if "2020年以来，是否出现过发热、乏力、咳嗽、腹泻等以下不适症状？" in list_txt:
+            index = list_txt.index("2020年以来，是否出现过发热、乏力、咳嗽、腹泻等以下不适症状？")
+            healthy = list_txt[(index+1)]
+
+        flu = ''
+        if "疫情期间流行病学史（对2020年以来本人离返京情况、参与聚餐聚会情况、进行过哪些活动以及春节期间本人去向等动态情况描述）：" in list_txt:
+            index = list_txt.index("疫情期间流行病学史（对2020年以来本人离返京情况、参与聚餐聚会情况、进行过哪些活动以及春节期间本人去向等动态情况描述）：")
+            flu = list_txt[(index + 1)]
+
+        first_date = ''
+        first_moon = ''
+        first_sun = ''
+        if "第1次" in list_txt:
+            index = list_txt.index("第1次")
+            if '□阴性' not in list_txt[index+1]:
+                first_date = list_txt[index+1]
+                first_moon = list_txt[index+2]
+                first_sun = list_txt[index+3]
+            else:
+                first_moon = list_txt[index + 1]
+                first_sun = list_txt[index + 2]
+
+        parent_fold = foldername
+        while os.path.dirname(parent_fold) != base_dir:
+            parent_fold = os.path.dirname(parent_fold)
+
+        fold = os.path.basename(parent_fold)
+
+        print(f'目录：{fold} 姓名:{name} 性别:{sex} 出生年月：{birth} 籍贯:{hometown} 身份证号：{id} ')
+        print(f'身高：{height} 体重：{weight} 血型：{blood} 所在单位及职务职级：{department} 关员号：{bj_id}')
+        print(f'{disease_history[0]} {disease_history[1]} {disease_history[2]} {disease_history[3]}')
+        print(f'健康状况:{healthy} 流行病学调查:{flu}')
+        print(f'第1次日期：{first_date} 第1次阴性结果：{first_moon} 第1次阳性结果：{first_sun}')
+
+        row = [fold, name, sex, birth, hometown, id, height, weight, blood, department, bj_id, disease_history[0],\
+               disease_history[1],disease_history[2],disease_history[3], healthy, flu, first_date, first_moon, first_sun]
+        with open('/Users/cx/Downloads/output.csv', 'a', encoding='utf-8-sig') as csv_f:
+            writer = csv.writer(csv_f)
+            writer.writerow(row)
 
     return
 
