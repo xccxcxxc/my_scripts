43c35e0c615561286b27c239421b2f822a6adcac
xccxcxxc <chenx212@gmail.com>
2020-05-21 19:06:43 +0800

    test

--- 1os_操作所有文件.py
+++ 1os_操作所有文件.py
@@ -4,11 +4,13 @@
 """
 import os
 import shutil
+import re
 
 # os.getcwd 有可能是执行命令的目录而不是文件所在目录
-base_dir = os.path.abspath(os.path.dirname(__file__))
+#base_dir = os.path.abspath(os.path.dirname(__file__))
+base_dir = "d:\\20机关党委"
 
-
+# 移动文件
 def move_to_current_dir(foldername, file):
     # 如果目标文件已经存在，不覆盖
     if not os.path.exists(os.path.join(base_dir, file)):
@@ -16,7 +18,7 @@ def move_to_current_dir(foldername, file):
         print(os.path.join(foldername, file), os.path.join(base_dir, file))
     return
 
-
+# 拷贝文件
 def copy_to_current_dir(foldername, file):
     # 如果目标文件已经存在，不覆盖
     if not os.path.exists(os.path.join(base_dir, file)):
@@ -24,7 +26,7 @@ def copy_to_current_dir(foldername, file):
         print(os.path.join(foldername, file), os.path.join(base_dir, file))
     return
 
-
+# 删除文件
 def delete_file(foldername, file, ext=''):
     if os.path.splitext(file)[-1] == ext:
         print(os.path.join(foldername, file))
@@ -36,12 +38,22 @@ def do_something_to_file(foldername, file):
     # 调用具体代码
     # move_to_current_dir(foldername, file)
     # copy_to_current_dir(foldername, file)
-    delete_file(foldername, file, ext='.pdf')
+    #delete_file(foldername, file, ext='.pdf')
+
+    # 打印特定后缀的文件
+    if os.path.splitext(file)[-1] == '.txt':
+        print(f'fold:{foldername}, file:{file}, name:{os.path.splitext(file)[0]}')
+
+    name = os.path.splitext(file)[0]
+    ext = os.path.splitext(file)[-1]
+    if ext == '.txt':
+        file =
+        f = open(file, encoding='utf-8')
     return
 
 
 # os.walk 返回：1 当前文件夹名称 2 当前文件夹的子文件夹字符串列表 3 当前文件夹的文件字符串列表
-for foldername, subfolders, filenames in os.walk('.'):
+for foldername, subfolders, filenames in os.walk(base_dir):
     for file in filenames:
         do_something_to_file(foldername, file)
 
