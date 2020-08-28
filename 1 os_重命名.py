a155cf0dbb3d6dc8c0999eaf6df287a4f34a6f79
xccxcxxc <chenx212@gmail.com>
2020-08-28 20:13:31 +0800

    rename

--- 1 os_重命名.py
+++ 1 os_重命名.py
@@ -1,9 +1,10 @@
 import os
-folder_path = "/Users/linzehua/Desktop/Python代码练习/08/09"
+folder_path = "/Volumes/Orico/001-100 国日双语 多国字幕"
 file_list = os.listdir(folder_path)
 # 切换到当前文件夹路径下
 os.chdir(folder_path)
 for old_name in file_list:
-    new_name =  old_name[0:-1]
-    os.rename(old_name, new_name)
-    pass
+    if os.path.splitext(old_name)[-1] == 'mkv1':
+        new_name = old_name[0:-1]
+        os.rename(old_name, new_name)
+        pass
