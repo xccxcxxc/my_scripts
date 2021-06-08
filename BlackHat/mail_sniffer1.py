d22e2f12e9027479c69504cac598af2f81220c82
xccxcxxc <chenx212@gmail.com>
2021-06-08 11:19:02 +0800

    增加mail_sniffer简单版本

---
+++ BlackHat/mail_sniffer1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from scapy.all import sniff
+
+def packet_callback(packet):
+    print(packet.show())
+
+def main():
+    sniff(prn=packet_callback, count=1)
+
+
+if __name__ == '__main__':
+    main()
