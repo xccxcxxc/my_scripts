cd4bf45cd312f8a40a43159af2a89116f30fe2af
xccxcxxc <chenx212@gmail.com>
2021-06-15 12:31:47 +0800

    所有 Season 和 Episode 命名均转为大写

--- video_rename_split.py
+++ video_rename_split.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
         ret = re.split(r'\W', old_name)
         if len(ret) > int(episode):
             if season == episode:
-                new_name = tv_name + '-' + ret[int(season)] + '.' + ret[-1]
+                new_name = tv_name + '-' + ret[int(season)].upper() + '.' + ret[-1]
             else:
-                new_name = tv_name + '-' + ret[int(season)] + ret[int(episode)] + '.' + ret[-1]
+                new_name = tv_name + '-' + ret[int(season)].upper() + ret[int(episode)].upper() + '.' + ret[-1]
             new_names.append(new_name)
             if not os.path.exists(new_name):
                 os.replace(old_name, new_name)
