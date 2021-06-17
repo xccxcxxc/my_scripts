20c4b3144fd6fd78d91f9d94eed2626d0ee4c06d
xccxcxxc <chenx212@gmail.com>
2021-06-17 12:48:36 +0800

    检测真实网站

--- BlackHat/5/mapper.py
+++ BlackHat/5/mapper.py
@@ -43,7 +43,40 @@ def chdir(path):
         os.chdir(this_dir)
 
 
+def test_remote():
+    while not web_paths.empty():
+        path = web_paths.get()
+        url = f'{TARGET}{path}'
+        time.sleep(2)
+        r = requests.get(url)
+        if r.status_code == 200:
+            answers.put(url)
+            sys.stdout.write('+')
+        else:
+            sys.stdout.write('x')
+
+        sys.stdout.flush()
+
+
+def run():
+    mythreads = list()
+    for i in range(THREADS):
+        print(f'Spawning thread {i}')
+        t = threading.Thread(target=test_remote())
+        mythreads.append(t)
+        t.start()
+
+    for thread in mythreads:
+        # 等待子线程结束，主线程才结束
+        thread.join()
+
+
 if __name__ == "__main__":
     with chdir("/home/kali/Downloads/wordpress"):
         gather_paths()
     input('Press return to continue.')
+    run()
+    with open('myanswer.txt', 'w') as f:
+        while not answers.empty():
+            f.write(f'{answers.get()}\n')
+    print('done.')
